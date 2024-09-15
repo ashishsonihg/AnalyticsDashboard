7abd537e6f8fc1309a7927dcbdd3ab942c8b4a90
Ashish Soni <ashish_soni@outlook.com>
2024-09-15 15:02:00 +0530

    more datatable added

---
+++ src/components/DataTable/index.jsx
@@ -0,0 +1,262 @@
+import React, { useState } from 'react'
+import {
+  CaretSortIcon,
+  ChevronDownIcon,
+  DotsHorizontalIcon
+} from '@radix-ui/react-icons'
+import {
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable
+} from '@tanstack/react-table'
+
+import { Button } from '../ui/button'
+import { mockData as data } from './mockData'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '../ui/dropdown-menu'
+import { Input } from '../ui/input'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '../ui/table'
+
+// Song Name
+// Artist
+// Date Streamed
+// Stream Count
+// User ID
+// function to convert date string to beutiful date
+const formatDate = (date) => {
+  const options = { year: 'numeric', month: 'short', day: 'numeric' }
+  return new Date(date).toLocaleDateString('en-US', options)
+}
+export const columns = [
+  {
+    accessorKey: 'songName',
+    size: 30,
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Song
+          <CaretSortIcon className='ml-2 h-4 w-4' />
+        </Button>
+      )
+    },
+    cell: ({ row }) => (
+      <div className='capitalize px-4'>{row.getValue('songName')}</div>
+    )
+  },
+  {
+    accessorKey: 'artist',
+    size: 25,
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Artist
+          <CaretSortIcon className='ml-2 h-4 w-4' />
+        </Button>
+      )
+    },
+    cell: ({ row }) => (
+      <div className='capitalize px-4'>{row.getValue('artist')}</div>
+    )
+  },
+  {
+    accessorKey: 'dateStreamed',
+    size: 20,
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Date Streamed
+          <CaretSortIcon className='ml-2 h-4 w-4' />
+        </Button>
+      )
+    },
+    cell: ({ row }) => (
+      <div className='px-4'>{formatDate(row.getValue('dateStreamed'))}</div>
+    )
+  },
+  {
+    accessorKey: 'streamCount',
+    size: 20,
+    header: ({ column }) => {
+      return (
+        <div className='text-right'>
+          <Button
+            variant='ghost'
+            onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+            className='pr-0'
+          >
+            Stream Count
+            <CaretSortIcon className='ml-2 h-4 w-4' />
+          </Button>
+        </div>
+      )
+    },
+    cell: ({ row }) => {
+      const streamCount = row.getValue('streamCount')
+      // format streamCount to have commas
+      const formattedStreamCount = streamCount.toLocaleString()
+      return <div className='text-right px-4'>{formattedStreamCount}</div>
+    }
+  },
+  {
+    id: 'actions',
+    enableHiding: false,
+    size: 5,
+    cell: ({ row }) => {
+      const payment = row.original
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='ghost' className='h-8 w-8 p-0'>
+              <span className='sr-only'>Open menu</span>
+              <DotsHorizontalIcon className='h-4 w-4' />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end'>
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    }
+  }
+]
+
+export default () => {
+  const [sorting, setSorting] = useState([])
+  const [columnFilters, setColumnFilters] = useState([])
+  const [columnVisibility, setColumnVisibility] = useState({})
+  const [rowSelection, setRowSelection] = useState({})
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection
+    }
+  })
+
+  return (
+    <div className='w-full border p-[10px]'>
+      <div className='flex items-center py-4'>
+        <Input
+          placeholder='Filter Song...'
+          value={table.getColumn('songName')?.getFilterValue() ?? ''}
+          onChange={event =>
+            table.getColumn('songName')?.setFilterValue(event.target.value)
+          }
+          className='max-w-sm'
+        />
+      </div>
+      <div className='rounded-md border'>
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map(headerGroup => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map(header => {
+                  return (
+                    <TableHead key={header.id} style={{width: `${header.getSize()}%`}}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  )
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map(row => (
+                <TableRow key={row.id}>
+                  {row.getVisibleCells().map(cell => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className='h-24 text-center'
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className='flex items-center justify-end space-x-2 py-4'>
+        <div className='space-x-2'>
+          <Button
+            variant='outline'
+            size='sm'
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant='outline'
+            size='sm'
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/DataTable/mockData.js
@@ -0,0 +1,251 @@
+export const mockData = [
+  {
+      "songName": "Blinding Lights",
+      "artist": "The Weeknd",
+      "dateStreamed": "2022-09-15",
+      "streamCount": 3550000,
+      "userId": 1,
+      "id": 0
+  },
+  {
+      "songName": "Watermelon Sugar",
+      "artist": "Harry Styles",
+      "dateStreamed": "2023-03-21",
+      "streamCount": 2800000,
+      "userId": 2,
+      "id": 1
+  },
+  {
+      "songName": "Levitating",
+      "artist": "Dua Lipa",
+      "dateStreamed": "2021-11-12",
+      "streamCount": 4120000,
+      "userId": 3,
+      "id": 2
+  },
+  {
+      "songName": "Good 4 U",
+      "artist": "Olivia Rodrigo",
+      "dateStreamed": "2023-05-06",
+      "streamCount": 1970000,
+      "userId": 4,
+      "id": 3
+  },
+  {
+      "songName": "Peaches",
+      "artist": "Justin Bieber",
+      "dateStreamed": "2023-01-24",
+      "streamCount": 2510000,
+      "id": 4
+  },
+  {
+      "songName": "Save Your Tears",
+      "artist": "The Weeknd",
+      "dateStreamed": "2023-07-13",
+      "streamCount": 4950000,
+      "id": 5
+  },
+  {
+      "songName": "Montero (Call Me By Your Name)",
+      "artist": "Lil Nas X",
+      "dateStreamed": "2023-04-02",
+      "streamCount": 3300000,
+      "id": 6
+  },
+  {
+      "songName": "Stay",
+      "artist": "The Kid LAROI, Justin Bieber",
+      "dateStreamed": "2022-08-30",
+      "streamCount": 4210000,
+      "id": 7
+  },
+  {
+      "songName": "Kiss Me More",
+      "artist": "Doja Cat ft. SZA",
+      "dateStreamed": "2023-06-11",
+      "streamCount": 2840000,
+      "id": 8
+  },
+  {
+      "songName": "Industry Baby",
+      "artist": "Lil Nas X ft. Jack Harlow",
+      "dateStreamed": "2021-10-07",
+      "streamCount": 3650000,
+      "id": 9
+  },
+  {
+      "songName": "Drivers License",
+      "artist": "Olivia Rodrigo",
+      "dateStreamed": "2022-02-14",
+      "streamCount": 4200000,
+      "id": 10
+  },
+  {
+      "songName": "Bad Habits",
+      "artist": "Ed Sheeran",
+      "dateStreamed": "2023-08-02",
+      "streamCount": 1730000,
+      "id": 11
+  },
+  {
+      "songName": "Heat Waves",
+      "artist": "Glass Animals",
+      "dateStreamed": "2021-11-23",
+      "streamCount": 4050000,
+      "id": 12
+  },
+  {
+      "songName": "Shivers",
+      "artist": "Ed Sheeran",
+      "dateStreamed": "2022-05-14",
+      "streamCount": 3420000,
+      "id": 13
+  },
+  {
+      "songName": "As It Was",
+      "artist": "Harry Styles",
+      "dateStreamed": "2023-07-19",
+      "streamCount": 3700000,
+      "id": 14
+  },
+  {
+      "songName": "Easy On Me",
+      "artist": "Adele",
+      "dateStreamed": "2023-06-28",
+      "streamCount": 2760000,
+      "id": 15
+  },
+  {
+      "songName": "Butter",
+      "artist": "BTS",
+      "dateStreamed": "2022-04-10",
+      "streamCount": 3540000,
+      "id": 16
+  },
+  {
+      "songName": "Ghost",
+      "artist": "Justin Bieber",
+      "dateStreamed": "2023-02-20",
+      "streamCount": 3170000,
+      "id": 17
+  },
+  {
+      "songName": "Need To Know",
+      "artist": "Doja Cat",
+      "dateStreamed": "2022-12-30",
+      "streamCount": 4300000,
+      "id": 18
+  },
+  {
+      "songName": "STAY",
+      "artist": "The Kid LAROI",
+      "dateStreamed": "2023-08-08",
+      "streamCount": 2810000,
+      "id": 19
+  },
+  {
+      "songName": "Fancy Like",
+      "artist": "Walker Hayes",
+      "dateStreamed": "2021-10-15",
+      "streamCount": 1830000,
+      "id": 20
+  },
+  {
+      "songName": "My Universe",
+      "artist": "Coldplay & BTS",
+      "dateStreamed": "2022-01-09",
+      "streamCount": 2200000,
+      "id": 21
+  },
+  {
+      "songName": "Cold Heart",
+      "artist": "Elton John & Dua Lipa",
+      "dateStreamed": "2023-04-14",
+      "streamCount": 3120000,
+      "id": 22
+  },
+  {
+      "songName": "Stay High",
+      "artist": "Britney Howard",
+      "dateStreamed": "2022-07-17",
+      "streamCount": 2450000,
+      "id": 23
+  },
+  {
+      "songName": "Good Days",
+      "artist": "SZA",
+      "dateStreamed": "2023-05-23",
+      "streamCount": 3940000,
+      "id": 24
+  },
+  {
+      "songName": "Levitating",
+      "artist": "Dua Lipa",
+      "dateStreamed": "2021-06-18",
+      "streamCount": 3050000,
+      "id": 25
+  },
+  {
+      "songName": "Don't Start Now",
+      "artist": "Dua Lipa",
+      "dateStreamed": "2022-03-09",
+      "streamCount": 4850000,
+      "id": 26
+  },
+  {
+      "songName": "Dance Monkey",
+      "artist": "Tones and I",
+      "dateStreamed": "2022-02-11",
+      "streamCount": 4400000,
+      "id": 27
+  },
+  {
+      "songName": "Positions",
+      "artist": "Ariana Grande",
+      "dateStreamed": "2023-01-28",
+      "streamCount": 4020000,
+      "id": 28
+  },
+  {
+      "songName": "Memories",
+      "artist": "Maroon 5",
+      "dateStreamed": "2023-03-04",
+      "streamCount": 3760000,
+      "id": 29
+  },
+  {
+      "songName": "Holy",
+      "artist": "Justin Bieber",
+      "dateStreamed": "2023-08-24",
+      "streamCount": 4120000,
+      "id": 30
+  },
+  {
+      "songName": "Mood",
+      "artist": "24kGoldn ft. iann dior",
+      "dateStreamed": "2022-09-07",
+      "streamCount": 2710000,
+      "id": 31
+  },
+  {
+      "songName": "Savage Love",
+      "artist": "Jawsh 685 & Jason Derulo",
+      "dateStreamed": "2021-08-21",
+      "streamCount": 3340000,
+      "id": 32
+  },
+  {
+      "songName": "Peaches",
+      "artist": "Justin Bieber",
+      "dateStreamed": "2023-04-05",
+      "streamCount": 3940000,
+      "id": 33
+  },
+  {
+      "songName": "MONTERO",
+      "artist": "Lil Nas X",
+      "dateStreamed": "2022-05-13",
+      "streamCount": 3210000,
+      "id": 34
+  }
+]
